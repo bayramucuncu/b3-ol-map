68f3980eed661139cf399874d22aed6663af660b
Bayram Üçüncü <ucuncubayram@gmail.com>
2020-04-24 00:15:07 +0300

    InteractionContainer update.

--- projects/b3-ol-map/src/lib/interactions/interaction-container/interaction-container.component.ts
+++ projects/b3-ol-map/src/lib/interactions/interaction-container/interaction-container.component.ts
@@ -16,21 +16,23 @@ export class InteractionContainerComponent implements OnInit {
 
   ngOnInit() {
 
-    if(!this.interactions) {
-      this.interactions = [
-        { name: "dragpan", title: "Drag pan" },
-        { name: "doubleclickzoom", title: "Double click", settings: { duration: 250, delta: 1 } },
-        { name: "pinchzoom", title: "Pich zoom", settings: { duration: 250 } },
-        { name: "pinchrotate", title: "Pich rotate", settings: { duration: 250, threshold: 0.3 } },
-        { name: "mousewheelzoom", title: "Mouse wheel interaction", settings: { duration: 250, timeout: 80, useAnchor: true, constrainResolution: false } }
-      ]
-    }
+    (!this.interactions) && (this.interactions = this.getDefaultInteractions());
 
     this.interactions = this.interactions || [];
   }
 
   onDragAndDrop($event: any): void {
     this.outDragAndDrop.emit($event);
+  }
+
+  private getDefaultInteractions(): any[] {
+    return [
+      { name: "dragpan", title: "Drag pan" },
+      { name: "doubleclickzoom", title: "Double click", settings: { duration: 250, delta: 1 } },
+      { name: "pinchzoom", title: "Pich zoom", settings: { duration: 250 } },
+      { name: "pinchrotate", title: "Pich rotate", settings: { duration: 250, threshold: 0.3 } },
+      { name: "mousewheelzoom", title: "Mouse wheel interaction", settings: { duration: 250, timeout: 80, useAnchor: true, constrainResolution: false } }
+    ]
   }
 
 }
