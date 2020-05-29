34fa1a54fe5c032b551ad82e4bb3aaad1953b828
Bayram Üçüncü <ucuncubayram@gmail.com>
2020-05-29 22:33:50 +0300

    index.ts created for extension

--- projects/b3-ol-map/src/lib/dynamic-component.contract.ts
+++
@@ -1,3 +0,0 @@
-export interface IDynamicComponent {
-    getComponentType(): string;
-}
--- projects/b3-ol-map/src/lib/index.ts
+++ projects/b3-ol-map/src/lib/index.ts
@@ -1,3 +1,1 @@
 export * from './b3-ol-map.component';
-export * from './dynamic-component.contract';
-export * from './dynamic-component.service';
