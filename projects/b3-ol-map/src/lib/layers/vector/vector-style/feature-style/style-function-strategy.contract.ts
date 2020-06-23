06c772580e3acce903f19a9d251f45f9137595aa
Bayram Üçüncü <ucuncubayram@gmail.com>
2020-06-23 15:03:03 +0300

    Text property converted to content and formatted view..

--- projects/b3-ol-map/src/lib/layers/vector/vector-style/feature-style/style-function-strategy.contract.ts
+++ projects/b3-ol-map/src/lib/layers/vector/vector-style/feature-style/style-function-strategy.contract.ts
@@ -6,90 +6,9 @@ export interface IStyleFunctionStragey {
     getStyleFunction(options: any): any;
 }
 
-export class DefaultViewFunctionStrategy implements IStyleFunctionStragey {
-    getStyleFunction(options: any): any {
-
-        let style = StyleManager.createStyle(options.viewOptions);
-
-        return (feature: Feature, resolution: number) => {
-            return style;
-        }
-    }
-}
-
-export class UniqueViewFunctionStrategy implements IStyleFunctionStragey {
-    getStyleFunction(options: any): any {
-
-        let style = StyleManager.createStyle(options.viewOptions);
-
-        return (feature: Feature, resolution: number) => {
-
-            let text = style.getText();
-
-            if (text) {
-                let textRotation = GeometryLabelRotationFactory.getCalculator(feature).getRotation();
-                text.setRotation(textRotation);
-                text.setText(this.getText(feature, resolution, options.viewOptions));
-            }
-
-            return style;
-        }
-    }
-
-    private getText(feature: Feature, resolution: number, options: any) {
-
-        let text = (options.text && options.text.property)
-            ? feature.get(options.text.property) && feature.get(options.text.property).toString()
-            : '';
-
-        if (options.text && options.text.maxResolution && options.text.maxResolution < resolution)
-            return '';
-
-        return text;
-    }
-}
-
-export class ClusterViewFunctionStrategy implements IStyleFunctionStragey {
-    getStyleFunction(options: any): any {
-
-        let min = options.viewOptions.thematicOptions ? Math.min(...options.viewOptions.thematicOptions.map((o: any) => o.min)) : undefined;
-        let max = options.viewOptions.thematicOptions ? Math.max(...options.viewOptions.thematicOptions.map((o: any) => o.max)) : undefined;
-
-        let styles = [];
-        let style = StyleManager.createStyle(options.viewOptions);
-
-        if (options.viewOptions.thematicOptions && options.viewOptions.thematicOptions.length > 0) {
-            options.viewOptions.thematicOptions.forEach((item: any) => {
-                let style = StyleManager.createStyle(item.viewOptions);
-
-                styles.push({ min: item.min, max: item.max, style: style });
-            })
-        }
-      
-        let defaultStyle = StyleManager.createStyle(options.viewOptions);
-  
-        return (feature: Feature, resolution: number) => {
-
-            let size = feature.get('features').length;
-
-            if (styles.length > 0) {
-                styles.forEach(item => {
-                    if ((item.min < size && size <= item.max))
-                        style = item.style;
-
-                    if ((min && max) && (size > max || size < min))
-                        style = defaultStyle;
-                })
-            }
-
-            style.getText() && style.getText().setText(size.toString());
-
-            return style;
-        }
-    }
-}
-
 export class ThematicViewFunctionStrategy implements IStyleFunctionStragey {
+    regex: any = /\{([^}]+)\}/g;
+
     getStyleFunction(options: any): any {
 
         let min = options.viewOptions.thematicOptions ? Math.min(...options.viewOptions.thematicOptions.map((o: any) => parseFloat(o.expression.min))) : undefined;
@@ -126,12 +45,12 @@ export class ThematicViewFunctionStrategy implements IStyleFunctionStragey {
             let style = defaultStyle;
 
             let isCluster = feature.get('features');
-         
+
             if (styles.length > 0) {
                 styles.forEach(item => {
                     let featureValue = feature.get(item.property);
 
-                    if(isCluster)
+                    if (isCluster)
                         featureValue = feature.get('features').length
 
                     if (item.comparison == "between" || isCluster) {
@@ -140,23 +59,23 @@ export class ThematicViewFunctionStrategy implements IStyleFunctionStragey {
 
                         if ((min && max) && (featureValue > max || featureValue < min))
                             style = defaultStyle;
-                    }else {
-                        if(item.comparison == "=" && featureValue == item.value){
+                    } else {
+                        if (item.comparison == "=" && featureValue == item.value) {
                             style = item.style;
                         }
-                        if(item.comparison == "!=" && featureValue != item.value){
+                        if (item.comparison == "!=" && featureValue != item.value) {
                             style = item.style;
                         }
-                        if(item.comparison == ">" && featureValue > parseFloat(item.value)){
+                        if (item.comparison == ">" && featureValue > parseFloat(item.value)) {
                             style = item.style;
                         }
-                        if(item.comparison == ">=" && featureValue >=  parseFloat(item.value)){
+                        if (item.comparison == ">=" && featureValue >= parseFloat(item.value)) {
                             style = item.style;
                         }
-                        if(item.comparison == "<" && featureValue <  parseFloat(item.value)){
+                        if (item.comparison == "<" && featureValue < parseFloat(item.value)) {
                             style = item.style;
                         }
-                        if(item.comparison == "<=" && featureValue <=  parseFloat(item.value)){
+                        if (item.comparison == "<=" && featureValue <= parseFloat(item.value)) {
                             style = item.style;
                         }
                     }
@@ -166,17 +85,17 @@ export class ThematicViewFunctionStrategy implements IStyleFunctionStragey {
                     if (text) {
                         let textRotation = GeometryLabelRotationFactory.getCalculator(feature).getRotation();
                         text.setRotation(textRotation);
-                        text.setText(this.getText(feature, resolution, options.viewOptions));
+                        text.setText(this.createText(feature, resolution, options.viewOptions));
                     }
                 })
             } else {
 
                 let text = style.getText();
-            
+
                 if (text) {
                     let textRotation = GeometryLabelRotationFactory.getCalculator(feature).getRotation();
                     text.setRotation(textRotation);
-                    text.setText(this.getText(feature, resolution, options.viewOptions));
+                    text.setText(this.createText(feature, resolution, options.viewOptions));
                 }
             }
 
@@ -184,15 +103,15 @@ export class ThematicViewFunctionStrategy implements IStyleFunctionStragey {
         }
     }
 
-    private getText(feature: Feature, resolution: number, options: any) {
+    private createText(feature: Feature, resolution: number, options: any) {
 
-        let text = (options.text && options.text.property)
-            ? feature.get(options.text.property) && feature.get(options.text.property).toString()
-            : '';
-
-        if(feature.get("features")){
+        if (feature.get("features")) 
             return feature.get("features").length.toString();
-        }
+
+        if (!(options.text || options.text.content))
+            return "";
+        
+        let text = options.text.content.replace(this.regex, (x: any, y: any) => feature.get(y) !== undefined ? feature.get(y).toString() : y)
 
         if (options.text && options.text.maxResolution && options.text.maxResolution < resolution)
             return '';
